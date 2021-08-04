387aa7481591e8ffea56b14b2ccb592235b60419
ly88321 <43890984+ly88321@users.noreply.github.com>
2021-08-04 09:17:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,5 +31,5 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-pack
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
 
 #额外软件包
-echo >> feeds.conf.default
-echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+#echo >> feeds.conf.default
+#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
