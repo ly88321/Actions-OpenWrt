0ee4cc32790e91476c8f370551454e5c808c8675
ly88321 <43890984+ly88321@users.noreply.github.com>
2021-08-04 12:33:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,9 +27,5 @@ echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >> feed
 # Add passwall feed source
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
 
-# Add dockerman feed source
-git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
-
 #额外软件包
-#echo >> feeds.conf.default
-#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
